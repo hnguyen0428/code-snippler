c620b1d52032c0fe2e462b84f1437fed88f40561
Hoang Nguyen <hnguyen0428@yahoo.com>
2018-09-23 23:41:04 -0700

    update dockerfile

--- backend/dockerbuild.sh
+++ backend/dockerbuild.sh
@@ -7,10 +7,10 @@ else
 fi
 
 
-docker build --build-arg ADMIN_KEY_ENC=${ADMIN_KEY_ENC} \
-	--build-arg CLIENT_KEY_ENC=${CLIENT_KEY_ENC} \
-	--build-arg MONGODB_DATABASE=${MONGODB_DATABASE} \
-	--build-arg MONGODB_URI=${MONGODB_URI} \
-	--build-arg MONGODB_USERNAME=${MONGODB_USERNAME} \
-	--build-arg MONGODB_PASSWORD=${MONGODB_PASSWORD} \
-	-t $TAG .
+docker build -t $TAG \
+	--build-arg ADMIN_KEY_ENC="${ADMIN_KEY_ENC}" \
+	--build-arg CLIENT_KEY_ENC="${CLIENT_KEY_ENC}" \
+	--build-arg MONGODB_DATABASE="${MONGODB_DATABASE}" \
+	--build-arg MONGODB_URI="${MONGODB_URI}" \
+	--build-arg MONGODB_USERNAME="${MONGODB_USERNAME}" \
+	--build-arg MONGODB_PASSWORD="${MONGODB_PASSWORD}" .
