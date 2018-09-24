63df0cf38cfa2a8d47bbf5a0b7ed361f90062e83
Hoang Nguyen <hnguyen0428@yahoo.com>
2018-09-23 22:58:45 -0700

    docker build script

---
+++ backend/dockerbuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	TAG=codesnippler_backend
+else
+	TAG=$1
+fi
+
+
+docker build --build-arg ADMIN_KEY_ENC=${ADMIN_KEY_ENC} \
+	--build-arg CLIENT_KEY_ENC=${CLIENT_KEY_ENC} \
+	--build-arg MONGODB_DATABASE=${MONGODB_DATABASE} \
+	--build-arg MONGODB_URI=${MONGODB_URI} \
+	--build-arg MONGODB_USERNAME=${MONGODB_USERNAME} \
+	--build-arg MONGODB_PASSWORD=${MONGODB_PASSWORD} \
+	-t $TAG .
